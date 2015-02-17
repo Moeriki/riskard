94f7e43b1b97696eef7ca6472e29c690e3250a57
Dieter Luypaert <dieterluypaert@gmail.com>
2015-02-17 13:22:34 +0100

    Added less for dp measurements

--- util.js
+++ util.js
@@ -1,7 +1,24 @@
 (function() {
     'use strict';
 
+    var ppi = 96;
+    $(function() {
+        var $body, $inch;
+
+        $body = $('body');
+        $inch = $('<div style="width:1in"></div>');
+
+        $body.append($inch);
+        ppi = $inch.width() || ppi;
+
+        $inch.remove();
+    });
+
     RK.Util = {
+
+        dpToPx: function(dp) {
+            return dp * ppi / 160;
+        },
 
         scrollToPage: function(nr) {
             var $page, offsetLeft;
